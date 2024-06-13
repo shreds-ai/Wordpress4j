9bebbde32f67dcbc2fb009fd7067eb5d314f3b9f
shredsai <contact@shreds.ai>
2024-06-13 14:19:41 +0200

    init project

---
+++ src/main/java/ai/shreds/wordpress4j/Wordpress4jApplication.java
@@ -0,0 +1,12 @@
+package ai.shreds.wordpress4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wordpress4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wordpress4jApplication.class, args);
+    }
+}
